1f16e964cc14232d3b958ebbf7e88637d9c365e2
voja <voja@gmail.com>
2022-05-26 11:47:40 +0200

    3 1 Dependency Injection

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,21 +1,22 @@
-using Core;
+using Core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<FruitOptions>(options =>
-{
-        options.Name = "watermelon";
-});
+builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
 
 var app = builder.Build();
 
-//app.MapGet("/fruit", async (HttpContext context, IOptions<FruitOptions> FruitOptions) =>
-//{
-//        FruitOptions options = FruitOptions.Value;
-//        await context.Response.WriteAsync($"{options.Name}, {options.Color}");
-//});
+//IResponseFormatter formatter = new TextResponseFormatter();
 
-app.UseMiddleware<FruitMiddleware>();
+app.MapGet("/formatter1", async (HttpContext context, IResponseFormatter formatter) =>
+{
+        await formatter.Format(context, "Formatter 1");
+});
+
+app.MapGet("/formatter2", async (HttpContext context, IResponseFormatter formatter) =>
+{
+        await formatter.Format(context, "Formatter 2");
+});
 
 app.MapGet("/", () => "Hello World!");
 
