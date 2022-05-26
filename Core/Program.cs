cbdec46fdecc1774bb60337339c5454a72cc69de
voja <voja@gmail.com>
2022-05-26 09:51:01 +0200

    2 4 Short-circuiting the Request Pipeline

--- Core/Program.cs
+++ Core/Program.cs
@@ -5,8 +5,14 @@ var app = builder.Build();
 
 app.Use(async (context, next) =>
 {
-        await next();
-        await context.Response.WriteAsync($"\n Status code: { context.Response.StatusCode }");
+        if (context.Request.Path == "/short")
+        {
+                await context.Response.WriteAsync("Request short-circuited");
+        }
+        else
+        {
+                await next();
+        }
 });
 
 app.UseMiddleware<Middleware>();
