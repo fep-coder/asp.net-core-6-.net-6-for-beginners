655425228eac6d38bb4ee3d67cbe17fcd2ea6d33
voja <voja@gmail.com>
2022-05-26 09:14:53 +0200

    2 1 Custom Middleware

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+        if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+        {
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Custom Middleware \n");
+        }
+        await next();
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
