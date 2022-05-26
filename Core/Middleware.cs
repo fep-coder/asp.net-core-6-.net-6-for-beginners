fcde3110a27f320f5db954d27332324260033304
voja <voja@gmail.com>
2022-05-26 09:44:31 +0200

    2 2 Defining Middleware using a Class

---
+++ Core/Middleware.cs
@@ -0,0 +1,27 @@
+﻿namespace Core
+{
+        public class Middleware
+        {
+                private RequestDelegate _next;
+
+                public Middleware(RequestDelegate next)
+                {
+                        _next = next;
+                }
+
+                public async Task Invoke(HttpContext context)
+                {
+                        if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+                        {
+                                if (!context.Response.HasStarted)
+                                {
+                                        context.Response.ContentType = "text/plain";
+                                }
+
+                                await context.Response.WriteAsync("Class-based Middleware \n");
+                        }
+
+                        await _next(context);
+                }
+        }
+}
