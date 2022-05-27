78aed9f9645206f7aa46c76820049ba06fe57131
voja <voja@gmail.com>
2022-05-27 12:35:22 +0200

    4 10 Detecting HTTPS

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,26 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(options =>
-{
-        options.IdleTimeout = TimeSpan.FromMinutes(30);
-        options.Cookie.IsEssential = true;
-});
-
 var app = builder.Build();
 
-app.UseSession();
-
-app.MapGet("/session", async context =>
+app.MapGet("/https", async context =>
 {
-        int counter = (context.Session.GetInt32("counter") ?? 0) + 1;
-
-        context.Session.SetInt32("counter", counter);
-
-        await context.Session.CommitAsync();
-
-        await context.Response.WriteAsync($"Session: {counter}");
+        await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps}");
 });
 
 app.MapGet("/", () => "Hello World!");
