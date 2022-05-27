0e67bd436afa3a88a4508f35761ed3a5602d50e1
voja <voja@gmail.com>
2022-05-27 10:29:31 +0200

    4 4 Getting the environment

--- Core/Program.cs
+++ Core/Program.cs
@@ -11,6 +11,14 @@ app.MapGet("/config", async (HttpContext context, IConfiguration config) =>
 {
         string defaultDebug = config["Logging:LogLevel:Default"];
         await context.Response.WriteAsync(defaultDebug);
+
+        string environment = config["ASPNETCORE_ENVIRONMENT"];
+        await context.Response.WriteAsync(environment);
+
+        if (app.Environment.IsDevelopment())
+        {
+                await context.Response.WriteAsync("IsDevelopment");
+        }
 });
 
 app.UseMiddleware<FruitMiddleware>();
