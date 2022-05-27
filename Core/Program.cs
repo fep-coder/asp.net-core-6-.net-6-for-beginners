0b08ad8d412496ea38c799b6327cb46dd0c57157
voja <voja@gmail.com>
2022-05-27 10:08:15 +0200

    4 2 Accessing Configuration Settings

--- Core/Program.cs
+++ Core/Program.cs
@@ -2,6 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapGet("/config", async (HttpContext context, IConfiguration config) =>
+{
+        string defaultDebug = config["Logging:LogLevel:Default"];
+        await context.Response.WriteAsync(defaultDebug);
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
