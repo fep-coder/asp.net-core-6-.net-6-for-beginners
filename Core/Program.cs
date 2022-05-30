f258b4fd5bed1e918801ba656ff0c4c0a22e03ef
voja <voja@gmail.com>
2022-05-30 10:28:57 +0200

    6 1 Creating a Simple Web Service

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,5 +1,7 @@
 using Core.Infrastructure;
+using Core.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,44 @@ builder.Services.AddDbContext<DataContext>(options =>
 });
 
 var app = builder.Build();
+
+const string BASEURL = "/api/products";
+
+app.MapGet($"{BASEURL}/{{id}}", async (HttpContext context, DataContext data) =>
+{
+        string id = context.Request.RouteValues["id"] as string;
+
+        if (id != null)
+        {
+                Product product = data.Products.Find(long.Parse(id));
+                if (product == null)
+                {
+                        context.Response.StatusCode = StatusCodes.Status404NotFound;
+                }
+                else
+                {
+                        context.Response.ContentType = "application/json";
+                        await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(product));
+                }
+        }
+});
+
+app.MapGet(BASEURL, async (HttpContext context, DataContext data) =>
+{
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync(JsonSerializer.Serialize<IEnumerable<Product>>(data.Products));
+});
+
+app.MapPost(BASEURL, async (HttpContext context, DataContext data) =>
+{
+        Product product = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
+        if (product != null)
+        {
+                await data.AddAsync(product);
+                await data.SaveChangesAsync();
+                context.Response.StatusCode = StatusCodes.Status200OK;
+        }
+});
 
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
