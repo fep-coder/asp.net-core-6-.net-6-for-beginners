8ebb876f001cbbf45f28a23b38f6f1efb7094b2b
voja <voja@gmail.com>
2022-05-30 11:08:44 +0200

    6 2 Creating a Controller

---
+++ Core/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Controllers
+{
+        [Route("api/[controller]")]
+        public class ProductsController : ControllerBase
+        {
+                // api/products
+                [HttpGet]
+                public IEnumerable<Product> GetProducts()
+                {
+                        return new Product[]
+                        {
+                                new Product() { Name = "Product #1" },
+                                new Product() { Name = "Product #2" }
+                        };
+                }
+
+                // api/products/1
+                [HttpGet("{id}")]
+                public Product GetProduct()
+                {
+                        return new Product() { Id = 1, Name = "Product #1" };
+                }
+        }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,7 +1,5 @@
 using Core.Infrastructure;
-using Core.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,45 +8,11 @@ builder.Services.AddDbContext<DataContext>(options =>
         options.UseSqlServer(builder.Configuration["ConnectionStrings:DbConnection"]);
 });
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-const string BASEURL = "/api/products";
-
-app.MapGet($"{BASEURL}/{{id}}", async (HttpContext context, DataContext data) =>
-{
-        string id = context.Request.RouteValues["id"] as string;
-
-        if (id != null)
-        {
-                Product product = data.Products.Find(long.Parse(id));
-                if (product == null)
-                {
-                        context.Response.StatusCode = StatusCodes.Status404NotFound;
-                }
-                else
-                {
-                        context.Response.ContentType = "application/json";
-                        await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(product));
-                }
-        }
-});
-
-app.MapGet(BASEURL, async (HttpContext context, DataContext data) =>
-{
-        context.Response.ContentType = "application/json";
-        await context.Response.WriteAsync(JsonSerializer.Serialize<IEnumerable<Product>>(data.Products));
-});
-
-app.MapPost(BASEURL, async (HttpContext context, DataContext data) =>
-{
-        Product product = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
-        if (product != null)
-        {
-                await data.AddAsync(product);
-                await data.SaveChangesAsync();
-                context.Response.StatusCode = StatusCodes.Status200OK;
-        }
-});
+app.MapControllers();
 
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
