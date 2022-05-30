88abc6f218560d5a930a7db7d4e08308f8cee7e3
voja <voja@gmail.com>
2022-05-30 09:16:00 +0200

    5 2 Creating Data Models

---
+++ Core/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Models
+{
+        public class Category
+        {
+                public long Id { get; set; }
+                public string Name { get; set; }
+
+                public IEnumerable<Product> Products { get; set; }
+        }
+}
---
+++ Core/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Models
+{
+        public class Product
+        {
+                public long Id { get; set; }
+                public string Name { get; set; }
+                [Column(TypeName = "decimal(8, 2)")]
+                public decimal Price { get; set; }
+
+                public long CategoryId { get; set; }
+                public Category Category { get; set; }
+        }
+}
