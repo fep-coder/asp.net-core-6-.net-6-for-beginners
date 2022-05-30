73e47ac02d72ab5c24c65bc4ed6bd0c1828667c7
voja <voja@gmail.com>
2022-05-30 09:23:42 +0200

    5 3 DbContext class

---
+++ Core/Infrastructure/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Infrastructure
+{
+        public class DataContext : DbContext
+        {
+                public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+                public DbSet<Product> Products { get; set; }
+                public DbSet<Category> Categories { get; set; }
+        }
+}
