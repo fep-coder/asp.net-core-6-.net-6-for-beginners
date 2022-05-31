bd5210138103b8a5ee9ed07d5a4843be03c8877b
voja <voja@gmail.com>
2022-05-31 11:17:40 +0200

    6 13 Dealing with Related Data

---
+++ Core/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Infrastructure;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Controllers
+{
+        [ApiController]
+        [Route("api/[controller]")]
+        public class CategoriesController : ControllerBase
+        {
+                private DataContext _context;
+
+                public CategoriesController(DataContext context)
+                {
+                        _context = context;
+                }
+
+                // api/categories/1
+                [HttpGet("{id}")]
+                public async Task<Category> GetCategory(long id)
+                {
+                        Category category = await _context.Categories.Include(c => c.Products).FirstAsync(c => c.Id == id);
+                        if (category.Products != null)
+                        {
+                                foreach (Product product in category.Products)
+                                {
+                                        product.Category = null;
+                                }
+                        }
+
+                        return category;
+                }
+
+        }
+}
