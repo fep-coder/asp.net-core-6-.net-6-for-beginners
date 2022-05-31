d5a94aeab3a5e023447f31113ee50bed36093654
voja <voja@gmail.com>
2022-05-31 16:17:56 +0200

    7 2 Understanding Convention Routing

--- Core/Program.cs
+++ Core/Program.cs
@@ -14,7 +14,8 @@ var app = builder.Build();
 
 app.MapControllers();
 
-app.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedDatabase(context);
