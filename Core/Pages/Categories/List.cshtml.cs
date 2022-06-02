aa67d7d5c7bf85d7e15e559340b5ecb4c87c1dae
voja <voja@gmail.com>
2022-06-02 09:50:38 +0200

    8 2 Understanding Razor Pages Routing

---
+++ Core/Pages/Categories/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Core.Pages.Categories
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
