41980134bca49d741f3101b5579da7a28e79a350
voja <voja@gmail.com>
2022-06-02 09:23:41 +0200

    8 1 Using Razor Pages

---
+++ Core/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Core.Pages
+{
+        public class IndexModel : PageModel
+        {
+                public void OnGet()
+                {
+                }
+        }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -10,12 +10,15 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 app.MapControllers();
 
-//app.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
 app.MapDefaultControllerRoute();
+
+app.MapRazorPages();
 
 app.UseStaticFiles();
 
