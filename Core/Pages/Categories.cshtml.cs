0532e815436949332ed663cf44f2bcc177af91d4
voja <voja@gmail.com>
2022-06-02 11:22:11 +0200

    8 9 Creating Razor Pages Without Page Models

---
+++ Core/Pages/Categories.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Core.Pages
+{
+    public class CategoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
