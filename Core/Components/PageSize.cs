8a3a782f31d4e90098ff209687d3a4cf04cd15c4
voja <voja@gmail.com>
2022-06-02 12:19:43 +0200

    9 6 Creating Asynchronous View Components

---
+++ Core/Components/PageSize.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Components
+{
+        [ViewComponent]
+        public class PageSize : ViewComponent
+        {
+                public async Task<IViewComponentResult> InvokeAsync()
+                {
+                        HttpClient client = new();
+                        HttpResponseMessage response = await client.GetAsync("http://google.com");
+
+                        return View(response.Content.Headers.ContentLength);
+                }
+        }
+}
