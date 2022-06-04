0342a4bd21a754fb75cae461dfd233eed18d9966
voja <voja@gmail.com>
2022-06-04 11:26:36 +0200

    10 5 Inserting Content Around the Output Element

---
+++ Core/TagHelpers/ContentWrapperTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Core.TagHelpers
+{
+        [HtmlTargetElement("*", Attributes = "[wrap=true]")]
+        public class ContentWrapperTagHelper : TagHelper
+        {
+                public string BgColor { get; set; } = "danger";
+                public string WrapperString { get; set; }
+
+                public override void Process(TagHelperContext context, TagHelperOutput output)
+                {
+                        TagBuilder elem = new TagBuilder("div");
+                        elem.Attributes["class"] = "bg-primary text-white p-2 m-2";
+                        elem.InnerHtml.AppendHtml(WrapperString);
+
+                        output.PreElement.AppendHtml(elem);
+                        output.PostElement.AppendHtml(elem);
+                }
+        }
+}
