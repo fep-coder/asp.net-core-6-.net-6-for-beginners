161baa16fc1c14442bc5346f76b7571d31703c38
voja <voja@gmail.com>
2022-06-04 10:07:08 +0200

    10 1 Custom Tag Helpers

---
+++ Core/TagHelpers/TrTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Core.TagHelpers
+{
+        public class TrTagHelper : TagHelper
+        {
+                public string BgColor { get; set; } = "dark";
+                public string TextColor { get; set; } = "white";
+
+                public override void Process(TagHelperContext context, TagHelperOutput output)
+                {
+                        output.Attributes.SetAttribute("class", $"bg-{BgColor} text-center text-{TextColor}");
+                }
+        }
+}
