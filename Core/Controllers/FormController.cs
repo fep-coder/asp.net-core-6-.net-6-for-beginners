422e4c4b72e873f7231d13ef1d2510c3b040a470
voja <voja@gmail.com>
2022-06-06 11:53:53 +0200

    12 1 FormController

---
+++ Core/Controllers/FormController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Controllers
+{
+        public class FormController : Controller
+        {
+                private DataContext _context;
+
+                public FormController(DataContext context)
+                {
+                        _context = context;
+                }
+
+                public async Task<IActionResult> Index(long id = 1)
+                {
+                        return View(await _context.Products.FindAsync(id);
+                }
+
+                [HttpPost]
+                public IActionResult SubmitForm()
+                {
+                        foreach (string key in Request.Form.Keys)
+                        {
+                                TempData[key] = string.Join(", ", Request.Form[key]);
+                        }
+
+                        return RedirectToAction("Results");
+                }
+
+                public IActionResult Results()
+                {
+                        return View();
+                }
+        }
+}
