fe7706c792f4260a170472248b48e9b547858a41
voja <voja@gmail.com>
2022-06-07 11:24:49 +0200

    14 1 CrudController and Index

---
+++ Core/Controllers/CrudController.cs
@@ -0,0 +1,18 @@
+﻿using Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Controllers
+{
+        public class CrudController : Controller
+        {
+                private DataContext _context;
+
+                public CrudController(DataContext context)
+                {
+                        _context = context;
+                }
+
+                public IActionResult Index() => View(_context.Products.Include(p => p.Category));
+        }
+}
