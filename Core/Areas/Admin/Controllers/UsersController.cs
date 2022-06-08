c49a6b27e5f478efbe0cea2a54e7f1f8f1645a3e
voja <voja@gmail.com>
2022-06-08 11:25:31 +0200

    15 3 Listing users

--- Core/Areas/Admin/Controllers/UsersController.cs
+++ Core/Areas/Admin/Controllers/UsersController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core.Areas.Admin.Controllers
 {
         [Area("Admin")]
         public class UsersController : Controller
         {
+                private UserManager<IdentityUser> _userManager;
+
+                public UsersController(UserManager<IdentityUser> userManager)
+                {
+                        _userManager = userManager;
+                }
+
                 public IActionResult Index()
                 {
-                        return View();
+                        return View(_userManager.Users.ToList());
                 }
         }
 }
