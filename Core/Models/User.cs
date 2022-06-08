eba85b89aa55516446d8ce64bdd88212c9f504e5
voja <voja@gmail.com>
2022-06-08 11:42:47 +0200

    15 4 User Model

---
+++ Core/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+        public class User
+        {
+                [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
+                public string UserName { get; set; }
+                [Required, EmailAddress]
+                public string Email { get; set; }
+                [DataType(DataType.Password), Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
+                public string Password { get; set; }
+        }
+}
