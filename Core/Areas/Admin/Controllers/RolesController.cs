cabed31b5ad2da202b21295eeecafd805b16edc5
voja <voja@gmail.com>
2022-06-09 11:08:08 +0200

    15 10 Roles Controller

---
+++ Core/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Areas.Admin.Controllers
+{
+        [Area("Admin")]
+        public class RolesController : Controller
+        {
+                private UserManager<IdentityUser> _userManager;
+                private RoleManager<IdentityRole> _roleManager;
+
+                public RolesController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+                {
+                        _userManager = userManager;
+                        _roleManager = roleManager;
+                }
+
+                public IActionResult Index() => View(_roleManager.Roles);
+        }
+}
